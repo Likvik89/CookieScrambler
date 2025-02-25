0e4a26e5d2af024b8e5bb264992a408aa6507cd6
gitgrubbed <robinwangsgaardnielsen@gmail.com>
2025-02-25 09:34:29 +0100

    array of names of za cookis

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ document.getElementById("logCookies").addEventListener("click", () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.cookies.getAll({ url: tabs[0].url }, (cookies) => {
             console.log("Cookies for", tabs[0].url, ":", cookies);
+            console.log(cookies.map(cookie => cookie.name));
         });
     });
 });
